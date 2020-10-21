221706812a6dccec77b441a69c34751d33b959b0
Savage-E <Savage-E@users.noreply.github.com>
2020-10-22 01:21:48 +0300

    Changed migration

--- build/resources/main/db/V1__first_migration.sql
+++ build/resources/main/db/V1__first_migration.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.lecturers
 (
-    lect_id bigint NOT NULL,
+    lect_id serial NOT NULL,
     name character varying(20),
     surname character varying(20),
     email character varying(100),
@@ -9,7 +9,7 @@ CREATE TABLE public.lecturers
 
 CREATE TABLE public.students
 (
-    stud_id bigint NOT NULL,
+    stud_id serial NOT NULL,
     name character varying(20),
     surname character varying(20),
     email character varying(100),
@@ -18,17 +18,18 @@ CREATE TABLE public.students
 
 CREATE TABLE public.courses
 (
-    course_id bigint NOT NULL,
+    course_id serial NOT NULL,
     name character varying(100),
     description text,
     CONSTRAINT course_pkey PRIMARY KEY (course_id)
 );
 
-CREATE TABLE public.endrolled_students
+CREATE TABLE public.enrolled_students
 (
+    en_stud_id serial NOT NULL,
     stud_id bigint NOT NULL,
     course_id bigint NOT NULL,
-    status character varying(15),
+    CONSTRAINT enrolled_stud_id PRIMARY KEY (en_stud_id),
     CONSTRAINT stud_fkey FOREIGN KEY (stud_id)
         REFERENCES public.students (stud_id) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -40,14 +41,12 @@ CREATE TABLE public.endrolled_students
         ON DELETE NO ACTION
         NOT VALID
 );
-CREATE TABLE public.schedules
+CREATE TABLE public.subjects
 (
-    schedule_id bigint NOT NULL,
-    "time" time(6) without time zone,
-    date date,
+    subject_id serial NOT NULL,
     course_id bigint NOT NULL,
     lect_id bigint NOT NULL,
-    CONSTRAINT schedule_pkey PRIMARY KEY (schedule_id),
+    CONSTRAINT subject_pkey PRIMARY KEY (subject_id),
     CONSTRAINT course_fkey FOREIGN KEY (course_id)
         REFERENCES public.courses (course_id) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -60,18 +59,33 @@ CREATE TABLE public.schedules
         NOT VALID
 );
 
+CREATE TABLE public.schedules
+(
+    schedule_id serial NOT NULL,
+    "time" time(6) without time zone,
+    date date,
+    subject_id bigint NOT NULL,
+    CONSTRAINT schedule_pkey PRIMARY KEY (schedule_id),
+    CONSTRAINT subject_fkey FOREIGN KEY (subject_id)
+    REFERENCES public.subjects (subject_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID
+
+);
+
 CREATE TABLE public.attendance
 (
     schedule_id bigint NOT NULL,
-    stud_id bigint NOT NULL,
+    en_stud_id bigint NOT NULL,
     status character varying(15),
     CONSTRAINT schedule_fkey FOREIGN KEY (schedule_id)
         REFERENCES public.schedules (schedule_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
-    CONSTRAINT student_fkey FOREIGN KEY (stud_id)
-        REFERENCES public.students (stud_id) MATCH SIMPLE
+    CONSTRAINT enrolled_student_fkey FOREIGN KEY (en_stud_id)
+        REFERENCES public.enrolled_students (en_stud_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
@@ -79,30 +93,25 @@ CREATE TABLE public.attendance
 
 CREATE TABLE public.hometasks
 (
-    hometask_id bigint NOT NULL,
+    hometask_id serial NOT NULL,
     deadline_time time(6) without time zone,
     deadline_date date,
-    course_id bigint NOT NULL,
-    lecture_id bigint NOT NULL,
+    schedule_id bigint NOT NULL,
+    description text,
     CONSTRAINT hometask_pkey PRIMARY KEY (hometask_id),
-    CONSTRAINT course_fkey FOREIGN KEY (course_id)
-        REFERENCES public.courses (course_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT lecturer_fkey FOREIGN KEY (lecture_id)
-        REFERENCES public.lecturers (lect_id) MATCH SIMPLE
+    CONSTRAINT schedule_fkey FOREIGN KEY (schedule_id)
+        REFERENCES public.schedules (schedule_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
 );
 CREATE TABLE public.marks
 (
-    stud_id bigint NOT NULL,
+    en_stud_id bigint NOT NULL,
     hometask_id bigint NOT NULL,
     mark bigint,
-    CONSTRAINT student_fkey FOREIGN KEY (stud_id)
-        REFERENCES public.students (stud_id) MATCH SIMPLE
+    CONSTRAINT en_student_fkey FOREIGN KEY (en_stud_id)
+        REFERENCES public.enrolled_students (en_stud_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
